ef76253a34f173f61eff22261cbf34247e4f891f
rodgomes <rgomes@gmail.com>
2016-02-17 16:49:59 +0100

    moving some code from app.html to index.html

--- web/controllers/search_controller.ex
+++ web/controllers/search_controller.ex
@@ -1,16 +1,39 @@
 defmodule Picty.SearchController do
   use Picty.Web, :controller
   alias Picty.FlickrAPI
+  alias Timex.Date
 
   def search(conn, params) do
     cityName = Dict.get(params, "city")
     month = Dict.get(params, "month")
 
-    result = FlickrAPI.search(cityName, month)
+    result = FlickrAPI.search(cityName, get_period(month))
     render conn, "search.json", %{"result" => result}
   end
 
   def suggested_locations(conn, _params) do
       render conn, "locations.json", %{"result" => FlickrAPI.get_allowed_locations()}
+  end
+
+  defp get_period(month) do
+    # check the month. If it is before the current month, the period should be related
+    # to last year, otherwise use the current year
+    today = Date.now
+    selected_month = elem(Integer.parse(month), 0)
+    current_year = today.year
+    previous_year = current_year - 1
+
+    cond do
+      selected_month < today.month ->
+        get_tuple_date(current_year, selected_month)
+      selected_month >= today.month ->
+        get_tuple_date(previous_year, selected_month)
+    end
+  end
+
+  defp get_tuple_date(year, month) do
+      lastDay = Date.days_in_month(year, month)
+      {to_string(year) <> "-" <> to_string(month) <> "-" <> "01",
+      to_string(year) <> "-" <> to_string(month) <> "-" <> to_string(lastDay)}
   end
 end
--- web/services/flickr_service.ex
+++ web/services/flickr_service.ex
@@ -1,8 +1,7 @@
 defmodule Picty.FlickrAPI do
-  alias Timex.Date
 
-  def search(cityName, month) do
-    response = HTTPotion.get(mount_url(cityName, get_period(month)), [timeout: 20_000])
+  def search(cityName, period) do
+    response = HTTPotion.get(mount_url(cityName, period), [timeout: 20_000])
     #TODO check errors, etc
     response.body
   end
@@ -11,33 +10,18 @@ defmodule Picty.FlickrAPI do
     ["Amsterdam", "Paris", "Berlin", "Prague", "London", "Rio de Janeiro"]
   end
 
-  defp mount_url(cityName, period) do
+  defp mount_url(cityName, {from, to}) do
     api_key = Application.get_env(:picty, :flickr_key)
-    from = elem(period, 0)
-    to = elem(period, 1)
-    "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{api_key}&text=#{cityName}&min_taken_date=#{from}&max_taken_date=#{to}&format=json&nojsoncallback=1"
+    query_params = %{
+      :method => "flickr.photos.search",
+      :api_key => api_key,
+      :text => cityName,
+      :min_taken_date => from,
+      :max_taken_date => to,
+      :format => "json",
+      :nojsoncallback => 1
+    }
+    "https://api.flickr.com/services/rest/?" <> URI.encode_query(query_params)
   end
-
-  defp get_period(month) do
-    # check the month. If it is before the current month, the period should be related
-    # to last year, otherwise use the current year
-    today = Date.now
-    selected_month = elem(Integer.parse(month), 0)
-    current_year = today.year
-    previous_year = current_year - 1
-
-    cond do
-      selected_month < today.month ->
-        get_tuple_date(current_year, selected_month)
-      selected_month >= today.month ->
-        get_tuple_date(previous_year, selected_month)
-    end
-  end
-
-  defp get_tuple_date(year, month) do
-      {to_string(year) <> "-" <> to_string(month) <> "-" <> "01",
-      to_string(year) <> "-" <> to_string(month) <> "-" <> "28"}
-  end
-
 
 end
