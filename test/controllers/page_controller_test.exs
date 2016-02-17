9447c1694d06aa150ee23d2faab9da7c0420753b
rodgomes <rgomes@gmail.com>
2016-02-17 17:43:10 +0100

    starting playing with tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule Picty.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "PICTY"
   end
 end
--- test/views/page_view_test.exs
+++ test/views/page_view_test.exs
@@ -1,3 +1,12 @@
 defmodule Picty.PageViewTest do
   use Picty.ConnCase, async: true
+
+  import Phoenix.View
+
+  test "renders index.html" do
+    assert render_to_string(Picty.PageView, "index.html", []) =~
+           "Show me some pictures"
+  end
+
+
 end
